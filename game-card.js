41a527724d895bebbcaef5f55cf9780c37f54497
natalikalinina1 <kidmil@mail.ru>
2023-06-03 19:33:05 +0300

    difficulty level

---
+++ game-card.js
@@ -0,0 +1,37 @@
+
+import { renderTextLevel } from "./textWindowLevel.js";
+import { renderstLevel} from "./stLevel.js";
+
+
+const game = document.getElementById('game')
+
+const renderGame = () => {
+	renderTextLevel(game);
+};
+
+renderGame();
+
+const btnStart = document.querySelector(".start");
+const formHelloLevel = document.querySelector(".level__container");
+formHelloLevel.addEventListener("input", () => {
+    if (document.querySelector('input[name="level"]:checked').value === "") {
+        btnStart.disabled = true;
+    } else {
+        btnStart.disabled = false;
+    }
+});
+
+export let level = "";
+
+btnStart.addEventListener('click', () => {
+    level = document.querySelector('input[name="level"]:checked').value;
+    if (level === "1") {
+        renderstLevel(game);
+    } else if (level === "2") {
+        renderstLevel(game);
+    } else if (level === "3") {
+        renderstLevel(game);
+    }
+});
+
+
---
+++ stLevel.js
@@ -0,0 +1,85 @@
+
+import {level} from "./game-card.js";
+
+export const renderstLevel = (game) => {
+    const stLevelHtml = `<div class=" level__container">
+    <h2 class="level__text"> Сложность: ${level} !</h2> </div>`;
+
+    game.innerHTML = stLevelHtml;
+
+    setTimeout(() => {
+        game.innerHTML = `${gameHeader} <div class="game__cards">
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             <div class="game__cards-item"></div>
+             </div>`
+    }, 1200);
+}
+
+const gameHeader =
+    ` <div class="game__header">
+    <div class="game__time-box">
+    <div class="game__text-box">
+        <p class="game__text">min</p>
+        <p class="game__text">sek</p>
+    </div>
+    <div class="game__time"> </div>
+    </div>
+     <div class="buttonOver"> Начать заново </div>
+    </div>`; 
+
+const gameClosedCards = `
+    <div class="game1">
+    ${gameHeader}
+    <div class="game__cards">
+    </div>
+    </div>`;
+game.innerHTML = gameClosedCards;
+
+        // Запускаем таймер
+        let time = 0;
+        const gameTimer = setInterval(() => {
+            time++;
+
+            const minutes = Math.floor(time / 60).toString().padStart(2, '0');
+            const seconds = (time % 60).toString().padStart(2, '0');
+
+            const gameTimerElement = document.querySelector('.game__time');
+            gameTimerElement.textContent = `${minutes}.${seconds}`;
+        }, 1000);
+		
+		
+   
