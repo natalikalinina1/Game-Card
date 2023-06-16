1d38e055e54173dcdb7c33ad74bcfeeb43df2029
natalikalinina1 <kidmil@mail.ru>
2023-06-16 17:10:03 +0300

    TS

--- stLevel.ts
+++ stLevel.ts
@@ -42,27 +42,28 @@ export const renderstLevel = (game:any, cardsCount:number) => {
 
     let startTime:any, interval:any
 
-    const startTimer = () => {
-        startTime = Date.now()
-        interval = setInterval(() => {
-            const elapsedTime = Math.floor((Date.now() - startTime) / 1000)
-            const min = Math.floor(elapsedTime / 60)
-                .toString()
-                .padStart(2, '0')
-            const sec = (elapsedTime % 60).toString().padStart(2, '0')
-            document.querySelector('.game__time').innerHTML = `${min}:${sec}`
-        }, 1000)
-    }
-
-    const stopTimer = () => {
-        clearInterval(interval)
-    }
-    const resetTimer = () => {
-        stopTimer()
-        document.querySelector('.game__time').innerHTML = '00:00'
-
-        startTimer()
-    }
+    const startTimer = (): void => {
+      let startTime: any = Date.now();
+      let interval: any = setInterval(() => {
+        const elapsedTime: number = Math.floor((Date.now() - startTime) / 1000);
+        const min: string = Math.floor(elapsedTime / 60)
+          .toString()
+          .padStart(2, "0");
+        const sec: string = (elapsedTime % 60).toString().padStart(2, "0");
+        const time: string = `${min}:${sec}`;
+        document.querySelector(".game__time")!.innerHTML = time;
+      }, 1000);
+    };
+    
+    const stopTimer = (): void => {
+      clearInterval(interval);
+    };
+    
+    const resetTimer = (): void => {
+      stopTimer();
+      document.querySelector(".game__time")!.innerHTML = "00:00";
+      startTimer();
+    };
 
     for (let i = 0; i < cardsCount; i++) {
         const randomCard = cards[Math.floor(Math.random() * cards.length)]
@@ -109,97 +110,81 @@ export const renderstLevel = (game:any, cardsCount:number) => {
     startTimer()
 
     setTimeout(() => {
-        game.innerHTML = closedCards
-        document.querySelector('.buttonOver').addEventListener('click', () => {
-            resetTimer() // добавляем вызов функции остановки таймера
-            renderstLevel(game, cardsCount) // вызываем функцию renderstLevel заново, чтобы начать игру сначала
-        })
-        const cardElements = document.querySelectorAll('.game-go__cards-item')
-        let previousCard: HTMLElement | null = null;// переменная для хранения предыдущей открытой карты
-        let matchedCardsCount = 0 // переменная для подсчета количества уже совпавших карт
-        cardElements.forEach((card) => {
-            card.addEventListener('click', (event) => {
-                const clickedCard = event.currentTarget as HTMLElement
-                const cardIndex = clickedCard.getAttribute('data-index')
-                const selectedCard = cards[cardIndex]
-                clickedCard.innerHTML = `
-          <img src="./images/${selectedCard}.png" alt="${selectedCard}">
-        `
-                if (previousCard === null) {
-                    previousCard = clickedCard // если это первая открытая карта, сохраняем ее в переменную
-                } else {
-                    const previousCardIndex =
-                        previousCard.getAttribute('data-index')
-                    const previousCardValue = cards[previousCardIndex]
-                    if (
-                        selectedCard.charAt(0) === previousCardValue.charAt(0)
-                    ) {
-                        // если первая буква или число совпала с предыдущей, то удаляем обработчик клика и увеличиваем счетчик совпавших карт
-                        previousCard.removeEventListener('click', () => {})
-                        clickedCard.removeEventListener('click', () => {})
-                        previousCard = null
-                        matchedCardsCount++
-                        if (matchedCardsCount === cardsCount / 2) {
-                            // если все карты совпали, то выводим сообщение о победе
-                            stopTimer()
-                            const time =
-                                document.querySelector('.game__time').innerHTML
-                            const resultTable = `
-              <div class="result">
-              <div class="result-table">
-                <img src="./images/win.png" alt='win'>
-                <h2 class="result__status"> Вы выиграли!</h2>
-                <p class ="result__time-text">Затраченное время </p>
-                <p class = "result__time">${time}</p>
-                <button class="result__button_again"> Начать заново </button>
-              </div>
-              </div>
-            `
-                            game.innerHTML = resultTable
-                            const againButton = document.querySelector(
-                                '.result__button_again'
-                            ) // перемещаем определение кнопки внутрь условия, чтобы она была доступна только после победы
-                            againButton.addEventListener('click', () => {
-                                renderstLevel(game, cardsCount) // вызываем функцию renderstLevel заново, чтобы начать игру сначала
-                            })
-                        }
-                    } else if (
-                        matchedCardsCount === cardsCount / 3 &&
-                        selectedCard !== previousCardValue
-                    ) {
-                        // если количество уже совпавших карт равно 1/3 от общего числа и они не совпали, то выводим сообщение о проигрыше и затраченном времени
-                        stopTimer()
-                        const time =
-                            document.querySelector('.game__time').innerHTML
-                        const resultTable = `
-            <div class="result">
-              <div class="result-table">
-              <img src="./images/loss.png" alt='loss'>
-                <h2 class="result__status">Вы проиграли!</h2>
-                <p class ="result__time-text">Затраченное время </p>
-                <p class = "result__time">${time}</p>
-                <button class= "result__button_again"> Начать заново </button>
-              </div>
-              </div>
-            `
+      game.innerHTML = closedCards;
+      document.querySelector('.buttonOver')?.addEventListener('click', () => {
+        resetTimer();
+        renderstLevel(game, cardsCount);
+      });
 
-                        game.innerHTML = resultTable
-                        const againButton = document.querySelector(
-                            '.result__button_again'
-                        ) // перемещаем определение кнопки внутрь условия, чтобы она была доступна только после победы
-                        againButton.addEventListener('click', () => {
-                            renderstLevel(game, cardsCount) // вызываем функцию renderstLevel заново, чтобы начать игру сначала
-                        })
-                    } else {
-                        // иначе закрываем обе карты через 1 секунду и сбрасываем предыдущую открытую карту
-                        setTimeout(() => {
-                            clickedCard.innerHTML = ''
-                            previousCard.innerHTML = ''
-                            //previousCard = null;
-                        }, 1000)
-                    }
+
+      const cardElements = document.querySelectorAll('.game-go__cards-item');
+      let previousCard: HTMLElement | null = null;
+      let matchedCardsCount = 0;
+    
+      cardElements.forEach((card: Element) => {
+        card.addEventListener('click', (event: Event) => {
+          const clickedCard = event.currentTarget as HTMLElement;
+          const cardIndex = clickedCard.getAttribute('data-index');
+          const selectedCard = cards[Number(cardIndex as string)];
+    
+          clickedCard.innerHTML = `
+            <img src="./images/${selectedCard}.png" alt="${selectedCard}">
+          `;
+    
+          if (previousCard === null) {
+            previousCard = clickedCard;
+          } else {
+            const previousCardIndex = previousCard.getAttribute('data-index');
+            const previousCardValue = cards[Number(previousCardIndex as string)];
+    
+            if (selectedCard[0] === previousCardValue[0]) {
+              previousCard.removeEventListener('click', () => {});
+              clickedCard.removeEventListener('click', () => {});
+              previousCard= null;
+              matchedCardsCount++;
+    
+              if (matchedCardsCount === cardsCount / 2) {
+                stopTimer();
+                const time = document.querySelector('.game__time')?.innerHTML;
+                const resultTable = `
+                  <div class="result">
+                    <div class="result-table">
+                      <img src="./images/win.png" alt='win'>
+                      <h2 class="result__status"> Вы выиграли!</h2>
+                      <p class ="result__time-text">Затраченное время </p>
+                      <p class = "result__time">${time}</p>
+                      <button class="result__button_again"> Начать заново </button>
+                    </div>
+                  </div>
+                `;
+                game.innerHTML = resultTable
+    
+                const againButton = document.querySelector<HTMLButtonElement>(
+                  '.result__button_again'
+                );
+                if (againButton) {
+                  againButton.addEventListener('click', () => {
+                    renderstLevel(game, cardsCount);
+                  });
+                } else {
+                  console.log('againButton is not found');
                 }
-            })
-        })
-    }, 2000)
-}
+              } else {
+                setTimeout(() => {
+                  clickedCard.innerHTML = '';
+                  previousCard!.innerHTML = '';
+                }, 1000);
+              }
+            }
+          }
+        });
+      });
+    }, 2000);
+  }
+
+
+                           
+            
+
+                       
+                       
