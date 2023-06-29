9a139203b441ef71036c94213d68d3080120c93f
natalikalinina1 <kidmil@mail.ru>
2023-06-29 21:55:53 +0300

    коррект

--- textWindowLevel.ts
+++ textWindowLevel.ts
@@ -1,6 +1,6 @@
 export const renderTextLevel = (game:HTMLElement):void  => {
     const textLevelHtml = `<div class="level__container">
-        <h2 class="level__text">Выбери сложность</h2>
+        <h2 class="level__text">Выбери </h2>
         <form class="level__form">
             <div class="level__value-gap">
                 <label class="level__value">
